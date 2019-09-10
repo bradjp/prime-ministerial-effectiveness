a5d3f28be684ad4969c723dc75bc8ffee80201c8
Brad Phillips <bjrp@protonmail.com>
2019-09-10 16:11:54 +0000

    Added display_pm method

--- pm_effectiveness.rb
+++ pm_effectiveness.rb
@@ -3,6 +3,9 @@
 @effectiveness = { Thatcher: 0.000996, Major: 0.002348, Blair: 0.001096, Brown: 0.00274, Cameron: 0.004566,
 May: 0.030137, Johnson: 0.857143 }
 
+@defeats = { Thatcher: '4 in 11 years', Major: '6 in 7 years', Blair: '4 in 10 years', Brown: '3 in 3 years', Cameron: '10 in 6 years',
+May: '33 in 3 years', Johnson: '6 in one week' }
+
 def show_record
   puts @record
 end
@@ -17,11 +20,17 @@ pm_two = gets.downcase.capitalize.chomp
   puts "#{@effectiveness.key(x[0])} is #{(x[1]/x[0]).round} times more effective than #{@effectiveness.key(x[1])}."
 end
 
+def display_pm
+  puts "Choose a Prime Minister (Thatcher, Major, Blair, Brown, Cameron, May, Johnson)"
+  pm = gets.downcase.capitalize.chomp
+  puts "#{pm}'s record for parliamentary defeats: #{@defeats[pm.intern]}"
+end
+
 def menu
   puts "After Boris Johnson's record 6 defeats in one week- compare the effectiveness of Prime Ministers"
   puts ' '
   loop do
-  puts 'Choose 1 to show the record of all PMs, 2 to compare two PMs, or 3 to exit'
+  puts 'Choose 1 to show the record of all PMs; 2 to compare two PMs; or 3 to see an individual record. 4 to exit'
   input(gets.chomp)
   end
 end
@@ -34,6 +43,8 @@ def input(choice)
   when '2'
     compare_pms
   when '3'
+    display_pm
+  when '4'
     exit
   end
 end
